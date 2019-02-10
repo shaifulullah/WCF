e4d665145b204425b56f0dd5aa6df9a7890ad633
shaifulullah <shaiful.ullah@yahoo.com>
2019-02-10 17:31:29 -0500

    not sure about files

---
+++ WCFConsole/WCFConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference.Service1Client obj = new ServiceReference.Service1Client();
+            var result = obj.AddData(2, 3);
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
